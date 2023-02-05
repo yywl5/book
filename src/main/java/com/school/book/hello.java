7518d94b2073f63cce57c4a9701fbfe94b89c3d2
yywl5 <1419453530@qq.com>
2023-02-05 19:38:08 +0800

    begin

---
+++ src/main/java/com/school/book/hello.java
@@ -0,0 +1,13 @@
+package com.school.book;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+@Controller
+public class hello {
+    @GetMapping("/hello")
+    @ResponseBody
+    public String hello(){
+        return "hello,spring boot!";
+    }
+}
